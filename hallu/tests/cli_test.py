5c7905d40b55dcee5aecc74aa3af421f9ca18d90
Mallory Hancock <malloryhancock89@gmail.com>
2019-04-18 08:13:28 -0700

    put tests in more logical place

--- hallu/tests/cli_test.py
+++ hallu/tests/cli_test.py
@@ -3,11 +3,9 @@ import hallu.hallu
 import click
 from click.testing import CliRunner
 
-def test_hello_world():
+def test_no_args():
     runner = CliRunner()
     result = runner.invoke(hallu.hallu.cli)
     assert result.exit_code == 0
     assert result.output == "hallu y'all\n"
 
-if __name__ == '__main__':
-    test_hello_world()
