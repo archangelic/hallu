843b826d849f446e74e9c7f19a7136b3ff1d020f
Mallory Hancock <malloryhancock89@gmail.com>
2019-05-09 10:03:26 -0700

    basic project initiation

--- hallu/hallu.py
+++ hallu/hallu.py
@@ -1,4 +1,5 @@
 import click
+import os
 
 @click.group(invoke_without_command=True)
 @click.pass_context
@@ -7,3 +8,13 @@ def cli(ctx):
         click.echo("hallu y'all")
     else:
         pass
+
+@cli.command('init')
+@click.option('-n', '--name', prompt=True)
+@click.option('-d', '--directory', default=os.getcwd())
+def hallu_init(name, directory):
+    if 'settings.py' not in os.listdir(directory):
+        with open('settings.py', 'w') as settings:
+            settings.write(f'NAME = "{name}"\n')
+    click.echo(f'Project: "{name}" successfully initiated')
+
--- hallu/tests/cli_test.py
+++ hallu/tests/cli_test.py
@@ -3,9 +3,21 @@ import hallu.hallu
 import click
 from click.testing import CliRunner
 
+import os
+
 def test_no_args():
     runner = CliRunner()
     result = runner.invoke(hallu.hallu.cli)
     assert result.exit_code == 0
     assert result.output == "hallu y'all\n"
+
+def test_init_no_args():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(hallu.hallu.cli, ['init'], input="test_run\n")
+        assert result.exit_code == 0
+        assert result.output.split('\n')[-2] == 'Project: "test_run" successfully initiated'
+        assert 'settings.py' in os.listdir()
+        with open('settings.py', 'r') as s:
+            assert s.read() == 'NAME = "test_run"\n'
 
